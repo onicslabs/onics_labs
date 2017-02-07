92496e912dafbef7611177fe6d0d8499e9b43152
Onics Labs <info@onicslabs.com>
2017-02-07 02:05:23 +0000

    Added handlers for static files
    In an attempt to add Bootstrap to the application, static files
    are now handled by `StaticFilesHandler` class in Nickel.
    
    Minor bug fix to main.rs to deal with reorganizing files.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 #[macro_use] extern crate nickel;
 extern crate chrono;
 
 use std::collections::HashMap;
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, StaticFilesHandler};
 use chrono::*;
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.get("/*", middleware! { |_, res|
-        let utc: DateTime<UTC> = UTC::now();
-        let mut data = HashMap::new();
-        let footsig = utc.format("<em>%b</em> || Onics Labs, LLC || <em>%Y</em>").to_string(); 
-        data.insert("foot-sig", footsig); 
-        return res.render("assets/templates/landing.tpl", &data);
-    });
+    server.utilize(StaticFilesHandler::new("src/static"));
+
+    server.utilize(router!(
+        get "/" => |req, res| {
+            let utc: DateTime<UTC> = UTC::now();
+            let mut data = HashMap::new();
+            let ftsig = "<em>%b</em> || Onics Labs, LLC || <em>%Y</em>"; 
+            data.insert("foot-sig", utc.format(ftsig).to_string()); 
+            return res.render("templates/landing.tpl", &data);
+        }
+    ));
+    
+    server.utilize(router!(
+        get "/boot" => |req, res| {
+            let utc: DateTime<UTC> = UTC::now();
+            let mut data = HashMap::new();
+            let ftsig = "<em>%b</em> || Onics Labs, LLC || <em>%Y</em>"; 
+            data.insert("foot-sig", utc.format(ftsig).to_string()); 
+            return res.render("templates/boot.tpl", &data);
+        }
+    ));
 
     server.listen("0.0.0.0:4201");
 }
